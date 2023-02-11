cd4c225f7a0ecf8f8f1fa724080bff313e6c54d0
huashen <huashen361@gmail.com>
2023-02-11 17:13:33 +0800

    new运算符

--- src/03/test11.cpp
+++ src/03/test11.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
 
-class Person() {
+/**
+ *
+ */
+class Person {
 public:
     Person() {
         cout << "默认构造调用" << endl;    
@@ -18,6 +21,12 @@ public:
 
 void test01() {
     Person *p2 = new Person;
+    //所有new出来的对象 都会返回该类型的指针
+    //malloc 返回 void* 还要强转
+    //malloc会调用构造吗？ 不会  new会调用构造
+    // new 运算符  malloc 函数
+    //释放 堆区空间
+    // delete也是运算符 配合 new用  malloc 配合 free用
     delete p2;  
 }
 
@@ -25,4 +34,9 @@ void test02() {
     void *p = new Person(10);
     //无法释放p
     delete p;
+}
+
+int main() {
+    test01();
+    return 0;
 }

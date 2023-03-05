4b5abb2ea5aec6993cc48dcbdeebcf99df08d356
huashen <huashen361@gmail.com>
2023-03-05 22:46:34 +0800

    继承

--- src/06/test04.cpp
+++ src/06/test04.cpp
@@ -21,11 +21,19 @@ public:
 
 class News :public BasePage //继承 News类继承于 BasePage类
 {
-
+public:
+    void content() {
+        cout << "新闻播放" << endl;
+    }
 };
 
 void test02() {
-    cout << "test" << endl;
+    cout << "新闻页内容" << endl;
+    News news;
+    news.header();
+    news.footer();
+    news.left();
+    news.content();
 }
 
 int main() {

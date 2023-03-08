c53185d6e9b1a3b953672aaa7e10b14d6b114d05
huashen <huashen361@gmail.com>
2023-03-08 10:40:01 +0800

    继承

--- src/06/test05.cpp
+++ src/06/test05.cpp
@@ -54,3 +54,34 @@ void myFunc2() {
     //s.m_A; 不能访问
 }
 
+
+//私有继承
+class Base3 {
+public:
+    int m_A;
+protected:
+    int m_B;
+private:
+    int m_C;        
+};
+
+class Son3 : private Base3 {
+public:
+    void func() {
+        // cout << m_C << endl;//基类中私有的属性 不可继承
+        cout << m_A << endl;//基类中公有的属性 可继承，还是private
+        cout << m_B << endl;//基类中保护的属性 可继承，还是private
+    }    
+};
+
+class GrandSon3 : public Son3 {
+public:
+    void myFunc() {
+        //cout << m_A << endl; //孙子类中 访问不到 m_A，因为在Son3中m_A已经是私有属性了
+    }    
+};
+
+int main() {
+    return 0;
+}
+

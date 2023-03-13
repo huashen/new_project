1b9dba46e42c43eceedfff8daf7e546a76b11940
huashen <huashen361@gmail.com>
2023-03-13 09:25:38 +0800

    多继承的概念及问题

---
+++ src/06/test10.cpp
@@ -0,0 +1,43 @@
+/**
+ * 多继承的概念及问题
+*/
+#include<iostream>
+using namespace std;
+
+class Base1 {
+public:
+    Base1() {
+        m_A = 10;
+    }
+public:
+    int m_A;        
+};
+
+class Base2 {
+public:
+    Base2() {
+        m_A = 20;
+    }
+public:
+    int m_A;    
+};
+
+class Son : public Base1, public Base2 {
+public:
+    int m_C;
+    int m_D;    
+};
+
+void test01() {
+    cout << sizeof(Son) << endl;
+    Son s1;
+    s1.m_A;
+
+    cout << s1.Base1::m_A << endl;
+    cout << s1.Base2::m_A << endl;
+}
+
+int main() {
+    test01();
+    return 0;
+}

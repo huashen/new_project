31f674a7aa0d74ff1b118f27321f5ba1e46021b4
huashen <huashen361@gmail.com>
2023-03-18 22:32:01 +0800

    静态联编和动态联编

--- src/07/test01.cpp
+++ src/07/test01.cpp
@@ -1,7 +1,36 @@
 /**
 * 静态联编和动态联编
 */
+#include<iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void speak() {
+        cout << "动物在说话" << endl;
+    }
+
+    virtual void eat() {
+        cout << "动物在吃饭" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void speak() {
+        cout << "小猫在说话" << endl;
+    }
+
+    virtual void eat() {
+        cout << "小猫在吃鱼" << endl;
+    }
+};
+
+void test01() {
+    Cat cat;
+}
 
 int main() {
+    test01();
     return 0;
 }
